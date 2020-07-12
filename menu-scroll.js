87f0215a4c1d17007e06708a0b7c705c6b3baba4
Primocode <@gmail.com>
2020-07-12 22:03:21 +0200

    Poprawki

--- menu-scroll.js
+++ menu-scroll.js
@@ -1,12 +1,12 @@
-function update() {
-    if ($(window).scrollTop() > 1) {
-      $('nav').attr('class', 'nav-active');
-      $('.navigation').attr('class', 'navigation-close');
-      $('.menu-bar-close').attr('class', 'menu-bar');
-    } else {
-      $('.nav-active').attr('class', 'nav');
-      $('.navigation-close').attr('class', 'navigation')
-      $('.menu-bar').attr('class', 'menu-bar-close')
-    }
-  }
-  setInterval(update, 0);
+// function update() {
+//     if ($(window).scrollTop() > 1) {
+//       $('nav').attr('class', 'nav-active');
+//       $('.navigation').attr('class', 'navigation-close');
+//       $('.menu-bar-close').attr('class', 'menu-bar');
+//     } else {
+//       $('.nav-active').attr('class', 'nav');
+//       $('.navigation-close').attr('class', 'navigation')
+//       $('.menu-bar').attr('class', 'menu-bar-close')
+//     }
+//   }
+//   setInterval(update, 0);
