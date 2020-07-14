bcb3e274cd827fb3ab42312fb21261a0e13f381f
Primocode <@gmail.com>
2020-07-14 18:06:07 +0200

    Dodanie menu mobilne

--- js/open-menu.js
+++ js/open-menu.js
@@ -1,12 +1,28 @@
-document.querySelector('#menu-hamburger').addEventListener('click', function() {
+const openMenu = () => {
     if (document.querySelector('#menu-icon-hamburger').className == "menu-icon") {
         document.querySelectorAll('#menu-icon-hamburger').forEach(item => {
             item.className = "menu-icon open";
+            document.querySelector('#navigation').className = "navigation navigation-mobile-open";
+            document.querySelector('#ul-nav').className = "ul-desktop ul-mobile";
+            document.querySelector('#nav-open').className = "nav nav-mobile";
         });
     }
     else {
         document.querySelectorAll('#menu-icon-hamburger').forEach(item => {
             item.className = "menu-icon";
+            document.querySelector('#navigation').className = "navigation"
+            document.querySelector('#ul-nav').className = "ul-desktop";
+            document.querySelector('#nav-open').className = "nav"
         }); 
     }
-})
+}
+
+document.querySelector('#menu-hamburger').addEventListener('click', openMenu);
+
+const closeMenuClick = () => {
+    openMenu();
+}
+
+
+document.querySelectorAll('#ul-nav > li > a').forEach(item => item.addEventListener('click', closeMenuClick))
+
