a2b0b112c9403583e0cb1be90dc288439b050dfe
Primocode <@gmail.com>
2020-09-26 18:04:41 +0200

    script improvement

--- js/open-menu.js
+++ js/open-menu.js
@@ -2,18 +2,18 @@ const openMenu = () => {
     if (document.querySelector('.menu-icon').className == "menu-icon") {
         document.querySelectorAll('.menu-icon').forEach(item => {
             item.className = "menu-icon open";
-            document.querySelector('#navigation').className = "navigation navigation-mobile-open";
-            document.querySelector('#ul-nav').className = "ul-desktop ul-mobile";
-            document.querySelector('#nav-open').className = "nav nav-mobile";
         });
+        document.querySelector('#navigation').className = "navigation navigation-mobile-open";
+        document.querySelector('#ul-nav').className = "ul-desktop ul-mobile";
+        document.querySelector('#nav-open').className = "nav nav-mobile";
     }
     else {
         document.querySelectorAll('.menu-icon').forEach(item => {
             item.className = "menu-icon";
-            document.querySelector('#navigation').className = "navigation"
-            document.querySelector('#ul-nav').className = "ul-desktop";
-            document.querySelector('#nav-open').className = "nav";
         }); 
+        document.querySelector('#navigation').className = "navigation"
+        document.querySelector('#ul-nav').className = "ul-desktop";
+        document.querySelector('#nav-open').className = "nav";
     }
 }
 document.querySelector('#menu-hamburger').addEventListener('click', openMenu);
